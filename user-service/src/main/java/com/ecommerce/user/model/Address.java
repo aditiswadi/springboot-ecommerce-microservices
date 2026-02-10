fa032a7cd9ea22dd25cec2ff2df99d849767de41
aditiswadi <aditiswadi19@gmail.com>
2026-02-10 09:59:25 +0700

    chore: remove external config repository from tracking

---
+++ user-service/src/main/java/com/ecommerce/user/model/Address.java
@@ -0,0 +1,16 @@
+package com.ecommerce.user.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+
+public class Address {
+    private Long id;
+    private String street;
+    private String city;
+    private String state;
+    private String country;
+    private String zipcode;
+}
